d8434f80183d60c514545efe489db55b06a6029f
k-qq <khori.qq@gmail.com>
2017-02-08 15:58:21 +0700

    Signed-off-by: k-qq <khori.qq@gmail.com>

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=pra_peminjamanruang',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
-];
